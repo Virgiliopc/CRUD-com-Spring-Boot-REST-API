47f1ea43115c2742710319f9117b3a6e61c75f21
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-05-30 23:53:51 -0300

    Create project springboot-rest-api
    Create project springboot-rest-api

---
+++ spring-boot-rest-api/src/main/java/br/com/springboot/spring_boot_rest_api/Application.java
@@ -0,0 +1,15 @@
+package br.com.springboot.spring_boot_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
