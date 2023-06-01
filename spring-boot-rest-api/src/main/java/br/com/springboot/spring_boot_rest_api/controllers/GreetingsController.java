f88c363f6fc324b6b0116f2a3c9570544e2ceaab
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-06-01 20:15:45 -0300

    database write test
    database write test

--- spring-boot-rest-api/src/main/java/br/com/springboot/spring_boot_rest_api/controllers/GreetingsController.java
+++ spring-boot-rest-api/src/main/java/br/com/springboot/spring_boot_rest_api/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.spring_boot_rest_api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,26 +8,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.spring_boot_rest_api.model.Usuario;
+import br.com.springboot.spring_boot_rest_api.repository.UsuarioRepository;
+
 /**
  *
  * A sample greetings controller to return greeting text
  */
 @RestController
 public class GreetingsController {
+	
+	@Autowired /* IC/CD ou CDI - Injeção de dependências */
+	private UsuarioRepository usuarioRepository;
     /**
      *
      * @param name the name to greet
      * @return greeting text
      */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/mostrarnome{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
+    	
+    	
         return "Hello " + name + "!";
     }
     
     @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	
+    	usuarioRepository.save(usuario); /*grava no banco de dados*/
+    	
 		return " Olá Mundo " + nome;    	
     }
 }
