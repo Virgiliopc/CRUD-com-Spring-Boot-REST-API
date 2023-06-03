93f5af79b0be1d282e066153ed440394886b30ba
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-06-02 21:48:55 -0300

    add method save Greeting.Controller.java
    add method save Greeting.Controller.java

--- spring-boot-rest-api/src/main/java/br/com/springboot/spring_boot_rest_api/controllers/GreetingsController.java
+++ spring-boot-rest-api/src/main/java/br/com/springboot/spring_boot_rest_api/controllers/GreetingsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -57,6 +59,15 @@ public class GreetingsController {
     	List<Usuario> usuarios = usuarioRepository.findAll();/*Executa a consulta no banco de dados*/
     	
     	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*Retorna a lista em JSON*/
+    }
+    
+    @PostMapping(value = "salvar")/*Mapeia a URL*/
+    @ResponseBody /*Descrição da resposta*/
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){ /*Recebe os dados para salvar*/
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED );
     }
     
 }
