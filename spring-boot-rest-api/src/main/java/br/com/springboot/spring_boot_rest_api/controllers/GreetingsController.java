641f0b251191dcc90bb97e194abc48950f990bbe
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-06-02 21:14:53 -0300

    add method API
    add method API

--- spring-boot-rest-api/src/main/java/br/com/springboot/spring_boot_rest_api/controllers/GreetingsController.java
+++ spring-boot-rest-api/src/main/java/br/com/springboot/spring_boot_rest_api/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.spring_boot_rest_api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,4 +49,14 @@ public class GreetingsController {
     	
 		return " Olá Mundo " + nome;    	
     }
+    
+    @GetMapping(value = "listatodos") /* Método de API*/
+    @ResponseBody /*Retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll();/*Executa a consulta no banco de dados*/
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*Retorna a lista em JSON*/
+    }
+    
 }
